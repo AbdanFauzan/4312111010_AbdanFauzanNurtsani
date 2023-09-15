5b8e72e8e22260736756dd3d17f0079e49609054
aqlika20 <abdanfzn@gmail.com>
2023-09-15 22:24:38 +0700

    Praktikum 4

---
+++ prak4_inkwell/android/app/src/main/kotlin/com/example/prak4_inkwell/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prak4_inkwell
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ prak4_text_form_validasi/android/app/src/main/kotlin/com/example/prak4_text_form_validasi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prak4_text_form_validasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
